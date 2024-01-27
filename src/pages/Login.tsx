efd306a8e6b9c17a0c87ce6fa053361e46df8884
pedram <peeeedii@gmail.com>
2024-01-27 23:55:29 +0330

    feat: add bare login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,28 @@
 export default function Login() {
-  return <div>Login</div>;
+  const handleForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const username = formData.get("username");
+    const password = formData.get("password");
+    fetch("http://127.0.0.1:3001/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    console.log(username, password);
+  };
+
+  return (
+    <form onSubmit={handleForm}>
+      <div>
+        <label htmlFor="username">Usrname: </label>
+        <input type="text" name="username" id="username" />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="text" name="password" id="password" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
