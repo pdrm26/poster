881124686484cc654894df7fef558cc4bc2973fd
pedram <peeeedii@gmail.com>
2024-02-15 19:08:12 +0330

    chore: better directory structure

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import Login from "./pages/Login";
 
 export default function App() {
   return (
-    <>
+    <div className="w-full h-screen bg-slate-300 flex justify-center items-center">
       <Login />
-    </>
+    </div>
   );
 }
+
 
 const container = document.getElementById("root");
 if (!container) throw new Error("I couldnt find the root element (;");
