0d02ab54ba857ca9f1f00cb95c3640afe083a764
pedram <peeeedii@gmail.com>
2024-02-15 19:08:28 +0330

    feat: add dotenv

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,25 +4,39 @@ export default function Login() {
     const formData = new FormData(event.currentTarget);
     const username = formData.get("username");
     const password = formData.get("password");
-    fetch("http://127.0.0.1:3001/login", {
+    fetch(`${import.meta.env.VITE_BASE_URL}/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
     });
-    console.log(username, password);
   };
 
   return (
-    <form onSubmit={handleForm}>
-      <div>
+    <form onSubmit={handleForm} className="space-y-4">
+      <div className="flex flex-col">
         <label htmlFor="username">Usrname: </label>
-        <input type="text" name="username" id="username" />
+        <input
+          type="text"
+          name="username"
+          id="username"
+          className="bg-slate-400/45 rounded-sm outline-none text-lg p-1"
+        />
       </div>
-      <div>
+      <div className="flex flex-col">
         <label htmlFor="password">Password: </label>
-        <input type="text" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="bg-slate-400/45 rounded-sm outline-none text-lg p-1"
+        />
       </div>
-      <button type="submit">Submit</button>
+      <button
+        type="submit"
+        className="py-2 px-4 rounded-md bg-slate-400 w-full"
+      >
+        Submit
+      </button>
     </form>
   );
 }
